24bd115896f18b0a922ceca55105a73a1a062189
Brian Vehige <brianvehige@gmail.com>
2021-10-17 16:36:54 -0700

    initial set-up for index.js

--- flicker_client/src/index.js
+++ flicker_client/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'react-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM, render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
